7cdd0c55ddc223031ae6f7f703c8ca7249a65ab4
Kevin Chen <xkevinchen@gmail.com>
2018-11-10 08:03:24 -0500

    Edit styling for listing show page and booking form

--- frontend/components/listing/listing_show.jsx
+++ frontend/components/listing/listing_show.jsx
@@ -85,51 +85,50 @@ class ListingShow extends React.Component {
     return (
       <div>
         <img className="listing-show-image" src={listing.img_url}></img>
-        <CreateBookingFormContainer
-          listing={listing}
-          prices={listing.prices}
-          numReviews={listing.review_ids.length}
-          guestNum={listing.guests} />
 
         <div className="listing-show-container">
-          <span style={{ color: "#734f21", fontSize: "12px", fontWeight: "800" }}>PRIVATE ROOM IN APARTMENT</span>
-          <div className="listing-show-title">{listing.title}</div>
-          <span className="listing-show-description">New York</span>
-          <div className="listing-show-content">
-            <div className="listing-show-commodities">
-              <i style={{ width: "12.5px", height: "19px", paddingRight: "8px" }} className="fas fa-user-friends"></i>&nbsp;{listing.guests}
-              <i style={{ width: "12.5px", height: "19px", paddingRight: "8px", paddingLeft: "12px" }} className="fas fa-warehouse"></i>&nbsp;{listing.bedrooms}
-              <i style={{ width: "12.5px", height: "19px", paddingRight: "8px", paddingLeft: "12px" }} className="fas fa-bed"></i>&nbsp;{listing.beds}
-              <i style={{ width: "12.5px", height: "19px", paddingRight: "8px", paddingLeft: "12px" }} className="fas fa-bath"></i>{listing.baths}
-            </div>
-            <div className="listing-show-description">
-              {listing.description}
-            </div>
-            <div className="separator-24"></div>
-            <div className="house-rules-header">Amenities</div>
-            <div className="amenities-container">
-              <ul>
+          <div className="listing-show-left">
+            <span style={{ color: "#734f21", fontSize: "12px", fontWeight: "800" }}>PRIVATE ROOM IN APARTMENT</span>
+            <div className="listing-show-title">{listing.title}</div>
+            <span className="listing-show-description">New York</span>
+            <div className="listing-show-content">
+              <div className="listing-show-commodities">
+                <i style={{ width: "12.5px", height: "19px", paddingRight: "8px" }} className="fas fa-user-friends"></i>&nbsp;{listing.guests}
+                <i style={{ width: "12.5px", height: "19px", paddingRight: "8px", paddingLeft: "12px" }} className="fas fa-warehouse"></i>&nbsp;{listing.bedrooms}
+                <i style={{ width: "12.5px", height: "19px", paddingRight: "8px", paddingLeft: "12px" }} className="fas fa-bed"></i>&nbsp;{listing.beds}
+                <i style={{ width: "12.5px", height: "19px", paddingRight: "8px", paddingLeft: "12px" }} className="fas fa-bath"></i>{listing.baths}
+              </div>
+              <div className="listing-show-description">
+                {listing.description}
+              </div>
 
-                <li>
-                  <svg className="amenities-icon" viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false">
-                    <path d="M22.5 23H21V1h1.5a.5.5 0 0 0 0-1H.5a.5.5 0 1 0 0 1H2v22H.5a.5.5 0 1 0 0 1h22a.5.5 0 0 0 0-1zM11 3v20H3V1h8v2zm9 20h-8V1h8v22zM4.146 10.854a.5.5 0 0 1 0-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 1 1-.708.708L7 9.707V14.5a.5.5 0 0 1-1 0V9.707l-1.146 1.147a.5.5 0 0 1-.708 0zm10 2a.5.5 0 1 1 .708-.708L16 13.293V8.5a.5.5 0 1 1 1 0v4.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2z" fillRule="evenodd">
-                    </path>
+              <div className="separator-24"></div>
+
+              <div className="house-rules-header">Amenities</div>
+              <div className="amenities-container">
+                <ul>
+
+                  <li>
+                    <svg className="amenities-icon" viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false">
+                      <path d="M22.5 23H21V1h1.5a.5.5 0 0 0 0-1H.5a.5.5 0 1 0 0 1H2v22H.5a.5.5 0 1 0 0 1h22a.5.5 0 0 0 0-1zM11 3v20H3V1h8v2zm9 20h-8V1h8v22zM4.146 10.854a.5.5 0 0 1 0-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 1 1-.708.708L7 9.707V14.5a.5.5 0 0 1-1 0V9.707l-1.146 1.147a.5.5 0 0 1-.708 0zm10 2a.5.5 0 1 1 .708-.708L16 13.293V8.5a.5.5 0 1 1 1 0v4.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2z" fillRule="evenodd">
+                      </path>
+                    </svg>
+                    Elevator
+                  </li>
+                  <li>
+                    <svg className="amenities-icon"viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false">
+                      <path d="m22 10v-2.5a1.5 1.5 0 0 0 -2.06-1.39 1.5 1.5 0 0 0 -2.94.39v3.5h-10v-3.5a1.5 1.5 0 0 0 -1.5-1.5c-.7 0-1.27.47-1.44 1.11a1.5 1.5 0 0 0 -2.06 1.39v2.5c-1.1 0-2 .89-2 2s .89 2 2 2v2.5a1.5 1.5 0 0 0 2.06 1.39 1.5 1.5 0 0 0 2.94-.39v-3.5h10v3.5a1.5 1.5 0 0 0 1.5 1.5c.7 0 1.27-.47 1.44-1.11a1.5 1.5 0 0 0 2.06-1.38v-2.51c1.11 0 2-.89 2-2s-.9-2-2-2zm-20 3c-.56 0-1-.45-1-1a1 1 0 0 1 1-1zm1.5 4a .5.5 0 0 1 -.5-.5v-9c0-.28.22-.5.5-.5s.5.22.5.5v9.01a.5.5 0 0 1 -.5.5zm2.5.5a.5.5 0 1 1 -1 0v-11a .5.5 0 1 1 1 0zm11-4.5h-10v-2h10zm2-5.5v10a .5.5 0 1 1 -1 0v-11a .5.5 0 1 1 1 0zm2 9.01a.5.5 0 0 1 -.5.5.5.5 0 0 1 -.5-.5v-9.01c0-.28.22-.5.5-.5s.5.22.5.5zm1-3.51v-2a1 1 0 0 1 1 1c0 .56-.44 1-1 1z" fillRule="evenodd">
+                      </path>
+                    </svg>
+                    Gym
+                  </li>
+                  <li>
+                    <svg className="amenities-icon"viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false"><path d="m10.5 0a .5.5 0 0 0 -.5.5v7a .5.5 0 0 1 -.49.5h-1.51v-7.5a.5.5 0 1 0 -1 0v7.5h-1.51a.5.5 0 0 1 -.49-.5v-7a .5.5 0 1 0 -1 0v7c0 .83.67 1.5 1.49 1.5h1.51v5c0 .03.01.06.02.09a1.49 1.49 0 0 0 -1.02 1.41v7c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5v-7c0-.66-.43-1.21-1.02-1.41.01-.03.02-.06.02-.09v-5h1.51a1.5 1.5 0 0 0 1.49-1.5v-7a .5.5 0 0 0 -.5-.5zm-2.5 15.5v7a .5.5 0 0 1 -.5.5.5.5 0 0 1 -.5-.5v-7c0-.28.22-.5.5-.5s.5.22.5.5zm11.5-15.5h-2c-1.4 0-2.5 1.07-2.5 2.5v7c0 1.43 1.1 2.5 2.5 2.5h1.5v2.09a1.49 1.49 0 0 0 -.5-.09c-.83 0-1.5.67-1.5 1.5v7c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5v-22.5zm-2 11c-.86 0-1.5-.63-1.5-1.5v-7c0-.87.65-1.5 1.5-1.5h1.5v10zm1.5 11.5a.5.5 0 0 1 -.5.5.5.5 0 0 1 -.5-.5v-7c0-.28.22-.5.5-.5s.5.22.5.5z" fillRule="evenodd"></path>
                   </svg>
-                  Elevator
+                  Kitchen
                 </li>
-                <li>
-                  <svg className="amenities-icon"viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false">
-                    <path d="m22 10v-2.5a1.5 1.5 0 0 0 -2.06-1.39 1.5 1.5 0 0 0 -2.94.39v3.5h-10v-3.5a1.5 1.5 0 0 0 -1.5-1.5c-.7 0-1.27.47-1.44 1.11a1.5 1.5 0 0 0 -2.06 1.39v2.5c-1.1 0-2 .89-2 2s .89 2 2 2v2.5a1.5 1.5 0 0 0 2.06 1.39 1.5 1.5 0 0 0 2.94-.39v-3.5h10v3.5a1.5 1.5 0 0 0 1.5 1.5c.7 0 1.27-.47 1.44-1.11a1.5 1.5 0 0 0 2.06-1.38v-2.51c1.11 0 2-.89 2-2s-.9-2-2-2zm-20 3c-.56 0-1-.45-1-1a1 1 0 0 1 1-1zm1.5 4a .5.5 0 0 1 -.5-.5v-9c0-.28.22-.5.5-.5s.5.22.5.5v9.01a.5.5 0 0 1 -.5.5zm2.5.5a.5.5 0 1 1 -1 0v-11a .5.5 0 1 1 1 0zm11-4.5h-10v-2h10zm2-5.5v10a .5.5 0 1 1 -1 0v-11a .5.5 0 1 1 1 0zm2 9.01a.5.5 0 0 1 -.5.5.5.5 0 0 1 -.5-.5v-9.01c0-.28.22-.5.5-.5s.5.22.5.5zm1-3.51v-2a1 1 0 0 1 1 1c0 .56-.44 1-1 1z" fillRule="evenodd">
-                    </path>
-                  </svg>
-                  Gym
-                </li>
-                <li>
-                  <svg className="amenities-icon"viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false"><path d="m10.5 0a .5.5 0 0 0 -.5.5v7a .5.5 0 0 1 -.49.5h-1.51v-7.5a.5.5 0 1 0 -1 0v7.5h-1.51a.5.5 0 0 1 -.49-.5v-7a .5.5 0 1 0 -1 0v7c0 .83.67 1.5 1.49 1.5h1.51v5c0 .03.01.06.02.09a1.49 1.49 0 0 0 -1.02 1.41v7c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5v-7c0-.66-.43-1.21-1.02-1.41.01-.03.02-.06.02-.09v-5h1.51a1.5 1.5 0 0 0 1.49-1.5v-7a .5.5 0 0 0 -.5-.5zm-2.5 15.5v7a .5.5 0 0 1 -.5.5.5.5 0 0 1 -.5-.5v-7c0-.28.22-.5.5-.5s.5.22.5.5zm11.5-15.5h-2c-1.4 0-2.5 1.07-2.5 2.5v7c0 1.43 1.1 2.5 2.5 2.5h1.5v2.09a1.49 1.49 0 0 0 -.5-.09c-.83 0-1.5.67-1.5 1.5v7c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5v-22.5zm-2 11c-.86 0-1.5-.63-1.5-1.5v-7c0-.87.65-1.5 1.5-1.5h1.5v10zm1.5 11.5a.5.5 0 0 1 -.5.5.5.5 0 0 1 -.5-.5v-7c0-.28.22-.5.5-.5s.5.22.5.5z" fillRule="evenodd"></path>
-                </svg>
-                Kitchen
-              </li>
               </ul>
+
               <ul>
                 <li>
                   <svg className="amenities-icon"viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false">
@@ -154,6 +153,7 @@ class ListingShow extends React.Component {
                 </li>
               </ul>
             </div>
+
             <div className="separator-24"></div>
             <div className="house-rules-header">Accessibility</div>
             <div className="amenities-container">
@@ -164,14 +164,18 @@ class ListingShow extends React.Component {
                 <li>Wide hallway clearance</li>
               </ul>
             </div>
+
             {reviewHeaderContainer}
             <div className="separator-24"></div>
             {reviewElements}
+
           </div>
+
           <div className="the-neighborhood-header">The neighborhood</div>
           <div className="show-page-map">
             <ListingMap singleListing={listing} lat={listing.lat} lng={listing.lng} fetchListing={this.props.fetchListing} />
           </div>
+
           <div className="the-neighborhood-header">Policies</div>
           <div className="house-rules-header">House Rules</div>
           <div className="review-content-text review-content-body">No pets</div>
@@ -183,7 +187,18 @@ class ListingShow extends React.Component {
           <div className="strict-text">Strict</div>
           <div className="review-content-text">Cancel up to 30 days before check-in and get a full refund.</div>
         </div>
+
+
+        <div className="listing-show-right">
+          <CreateBookingFormContainer
+            listing={listing}
+            prices={listing.prices}
+            numReviews={listing.review_ids.length}
+            guestNum={listing.guests} />
+        </div>
       </div>
+
+    </div>
     );
   }
 }
