a5c8bc053356e4df70dd027f616529f397e6986c
Kevin Chen <xkevinchen@gmail.com>
2018-11-14 01:51:36 -0500

    Test

---
+++ frontend/components/landing/landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Landing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+  }
+}
+
+export default Landing;
--- frontend/components/nav/navbar_container.jsx
+++ frontend/components/nav/navbar_container.jsx
@@ -4,9 +4,17 @@ import NavBar from './navbar';
 import { logout } from './../../actions/session_actions';
 import { openModal, closeModal } from './../../actions/modal_actions';
 
-const mapStateToProps = state => ({
-  currentUser: state.entities.users[state.session.currentUserId]
-});
+const mapStateToProps = state => {
+  if (state.session) {
+    return ({
+      currentUserId: state.session.currentUserId,
+      currentUser: state.entities.users[state.session.currentUserId]
+    });
+  } else {
+    return null;
+  }
+};
+
 const mapDispatchToProps = dispatch => ({
   logout: () => dispatch(logout()),
   openModal: modal => dispatch(openModal(modal)),
